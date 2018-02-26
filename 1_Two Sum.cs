7e8304e215999347222e2b5500837ea9d1a4f6ea
Luffy <liuys614@gmail.com>
2018-02-26 15:52:08 +0800

    upload missing code

--- 1_Two Sum.cs
+++ 1_Two Sum.cs
@@ -6,6 +6,7 @@ public class Solution {
         for( int i = 0; i < nums.Length; i++ ){
             for( int j = i + 1; j < nums.Length; j++ ){
                 if( nums[i] + nums[j] == target ){
+                    ans[0] = i;
                     ans[1] = j;
                     break;
                 }
